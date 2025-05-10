0a42e588f15a8ded2ff4cc2337cfe68b2c96de42
Idace <idaebeling@gmail.com>
2025-05-10 11:32:57 +0200

    route path "/"

--- react-group/src/App.jsx
+++ react-group/src/App.jsx
@@ -1,13 +1,16 @@
+import React from "react";
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import LoginForm from "./LoginForm";
+import Game from "./pages/Game";
 import Signup from "./pages/Signup";
 import ForgotPassword from "./pages/ForgotPassword";
-import Game from "./pages/Game";
+
 
 function App() {
   return (
     <Router>
       <Routes>
+        <Route path="/" element={<LoginForm/>}/>
         <Route path="/LoginForm" element={<LoginForm/>}/>
         <Route path="/Signup" element={<Signup />} />
         <Route path="/ForgotPassword" element={<ForgotPassword />}/>
