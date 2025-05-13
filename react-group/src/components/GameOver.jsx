3d1e30e836754cf3086ae474605a10626302d0d6
Brooke Brown <brooke.henriksson@outlook.com>
2025-05-13 13:29:48 +0200

    Added difficulty options and final score to game over menu

--- react-group/src/components/GameOver.jsx
+++ react-group/src/components/GameOver.jsx
@@ -1,7 +1,7 @@
 import RegularButton from "./ReqgularButton"
 import {useRef, useEffect} from 'react'
 
-export default function GameOver({handleClick}){
+export default function GameOver({handleClick, score}){
 
     const ref = useRef(null)
 
@@ -12,6 +12,7 @@ export default function GameOver({handleClick}){
       
     return (
         <div className="wrapper wrapper--accent bg-white/40 backdrop-blur-lg p-8 rounded-2xl shadow-xl w-full max-w-3/4 border border-white/20" ref={ref} tabIndex={-1}>
+            <h2 className="text-3xl bg-rose-500 p-4 rounded-4xl w-2/3">Final Score: {score} pts</h2>
             <p className="p--large">You've matched all the memory cards!</p>
             <RegularButton handleClick={handleClick}>
                 Play again?
